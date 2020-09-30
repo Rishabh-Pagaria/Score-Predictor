79a360db7267072a165345d10456c535e0863cfc
Rishabh-Pagaria <61039526+Rishabh-Pagaria@users.noreply.github.com>
2020-09-30 19:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     
-    output = prediction[0][0]
+    output = round(prediction[0][0],2)
+    
     
     return render_template('index.html', prediction_text = "Score Generated for no. of hours you have studied : {}".format(output))
 
