15789668c1d2d82e575dc1f45b5cd2cdd2874303
Rishabh-Pagaria <61039526+Rishabh-Pagaria@users.noreply.github.com>
2020-09-30 16:42:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 15:31:48 2020
+
+@author: Rishabh Pagaria
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    # For rendering results on HTML GUI
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    output = prediction[0][0]
+    
+    return render_template('index.html', prediction_text = "Score Generated for no. of hours you have studied : {}".format(output))
+
+if __name__ == "__main__":
+    app.run(debug = True)
